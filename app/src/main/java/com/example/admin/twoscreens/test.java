52ba0924b04d4db336440199576702cec3f1aa20
Bobbo03 <edde03@rocketmail.com>
2015-03-20 12:30:13 +0100

    Test
    Eddes projekt

---
+++ app/src/main/java/com/example/admin/twoscreens/test.java
@@ -0,0 +1,18 @@
+package com.example.admin.twoscreens;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Admin on 2015-03-20.
+ */
+public class test extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.knapp_test);
+
+    }
+
+
+
+}
